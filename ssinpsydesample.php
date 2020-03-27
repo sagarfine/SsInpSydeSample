487d08588df8cb4cd6c8ac226d89ca7165957826
saagarfine <sagarfine@gmail.com>
2020-03-27 23:46:20 +0530

    Description changes

--- ssinpsydesample.php
+++ ssinpsydesample.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Inpsyde Sample
 Plugin URI: https://inpsyde.com/
-Description: This is the plugin which send request to API from custom endpoint
-and show the result.
+Description: This plugin demonstrates, sending HTTP requests to third party API,
+process the received data and display.
 Version: 1.0
 Author: Sagar Shinde.
 Author URI: https://www.linkedin.com/in/sagarfine/
